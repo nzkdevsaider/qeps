c3f10a7d60a62efd64721c29ea1282239c139ce0
Sebastián <sebasmoraresu@gmail.com>
2023-10-20 22:56:58 -0500

    feat(db): v3

--- src/app/(onboard)/onboarding/page.js
+++ src/app/(onboard)/onboarding/page.js
@@ -1,6 +1,6 @@
 const page = () => {
     return (
-        <div className="flex flex-col w-2/5 h-3/4 bg-white p-5 space-y-4">
+        <div className="flex flex-col w-2/5 h-3/4 bg-white rounded-sm p-5 space-y-4">
             <h1 className="text-3xl font-bold">Empieza creando tu primera organización</h1>
             <p>Form</p>
         </div>
